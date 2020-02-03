80cf32f17173aa02b038d28cb944e934e213a806
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2020-02-03 15:55:50 -0500

    Adds the ability to save finished items

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: ''
+      text: '',
+      items: [],
+      done: []
     };
   }
   render() {
@@ -18,20 +20,55 @@ class App extends React.Component {
             placeholder="Type something here!"
           />
         </form>
+        <div>
+    {this.state.items.map((item, i) => {
+      return (
+        <div key={i}>
+        <input type="checkbox" onChange={() => this._storeDone(i)}></input>
+        <p onClick={() => {this._deleteText(i)}}>{item}</p>
+        </div>
+      );
+    })}
+        </div>
       </div>
     );
   }
 
   _handleSubmit = (event) => {
     event.preventDefault();
-    console.log('You just submitted');
+    // console.log(event);
+    let item = this.state.text;
+    this.state.text = "";
+    let newItems = [...this.state.items];
+    newItems.push(item);
+    this.setState({
+      items: newItems
+    })
   }
 
   _updateText = (event) => {
-    console.log(event.target.value);
-    // this.setState({
-    //   text: event.target.value
-    // })
+    // console.log(event.target.value);
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  _deleteText = (index) => {
+    const newItems = [...this.state.items];
+    newItems.splice(index, 1);
+    this.setState({
+      items: newItems
+    })
+  }
+
+  _storeDone = (i) => {
+    console.log('storing that the item was completed');
+    let item = this.state.items[i];
+    let newDone = [...this.state.done];
+    newDone.push(item);
+    this.setState({
+      done: newDone
+    })
   }
 }
 
